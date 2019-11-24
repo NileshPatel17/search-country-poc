e9f3fa64ba26d92b5dca793d3cdff475bad3ed23
Nilesh Patel <nilesh.nvs@hotmail.com>
2019-11-24 18:45:35 +0530

    remove console log

--- src/services/country-service.js
+++ src/services/country-service.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_URL = "https://restcountries-v1.p.rapidapi.com";
 
 export async function fetchCountries() {
-  console.log(process.env.REACT_APP_RAPIDAPI_KEY);
   const response = await axios.get(`${BASE_URL}/all`, {
     headers: {
       "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
